66829d098a8da552956d009500d618fb65b21b60
NikChao <nik.chaourov@gmail.com>
2018-08-04 22:38:25 +1000

    re-introduce db scripts, rds is up

--- webapi/app.py
+++ webapi/app.py
@@ -3,10 +3,10 @@ import falcon_cors
 import falcon_multipart.middleware
 
 from webapi.config import config
-# from webapi.database import init_database
+from webapi.database import init_database
 from webapi.middleware.cors import CorsMiddleware
 from webapi.middleware.empty_resource import EmptyResponseMiddleware
-# from webapi.middleware.database_connection import DatabaseConnectionMiddleware
+from webapi.middleware.database_connection import DatabaseConnectionMiddleware
 from webapi.error_handlers import register_error_handlers
 from webapi.media_handlers import register_media_handlers
 from webapi.routes import register_routes
